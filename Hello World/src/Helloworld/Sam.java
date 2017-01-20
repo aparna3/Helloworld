00b3cf01f8d1cdfe3c10b7407e5511b65e74ff07
mohammadsamkari <mohammadsamkari@209.42.106.215>
2017-01-19 19:51:33 -0500

    Added Code

---
+++ Hello World/src/Helloworld/Sam.java
@@ -0,0 +1,14 @@
+package Helloworld;
+
+public class Sam {
+
+	public Sam() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+	}
+
+}
